eaef0cb3c5646bb76d533a8db8e790e41f486ce6
IT21030062 <87383516+IT21030062@users.noreply.github.com>
2021-07-20 16:52:02 +0000

    completed

--- tute04.cpp
+++ tute04.cpp
@@ -11,19 +11,47 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-long Factorial(int no);
-long nCr(int n, int r);
+long factorial (int no);
+long nCr (int n , int r);
 
-int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+int main()
+{
+	int n,r;
+	
+	std :: cout << "Enter a value for n :";
+	std :: cin >> n;
+	
+	std :: cout << "Enter a value for r :";
+	std :: cin >> r;
+	
+	std :: cout << "nCr = ";
+	std :: cout << nCr(n,r);
+	std :: cout << std::endl;
+	
+	return 0;
+}
+
+long factorial (int no)
+{
+	int fac = 1; 
+	for(no;no>=1;no--)
+	{
+		 
+		fac = fac * no;
+		
+	}
+	return fac;
+	
+}
+
+long nCr (int n , int r)
+{
+	int nCr;
+	
+	nCr = factorial(n)/(factorial(r) * factorial(n - r));
+	return nCr;
+	
 }
